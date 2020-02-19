2511931a4b8a68c6bf838b8cf4bd10c7b79809d4
CrazyDi <43439123+CrazyDi@users.noreply.github.com>
2020-02-19 14:04:37 +0300

    commit

--- Pandas_Intro.py
+++ Pandas_Intro.py
@@ -1,2 +1,10 @@
-x = "Hello!"
-print(x)
+import pandas as pd
+import numpy as np
+
+student_perfomance = pd.read_csv('data/StudentsPerformance.csv')
+
+print(student_perfomance.describe())
+
+print(student_perfomance.dtypes)
+
+print(student_perfomance.shape)
